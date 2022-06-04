953e87672d262273aeb42b4cd475eef2ecc9fdaf
Edoardo-Pellegrini <pellegrini.ted@gmail.com>
2022-06-04 19:39:04 +0200

    week5_project commit 1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function darkmode(){
+    let body = document.querySelector('body');
+    body.style.backgroundColor = 'rgb(16, 2, 73)';
+    let servizi = document.getElementById('services');
+    servizi.style.color = 'white';
+    let rev = document.getElementById('rewievs');
+    rev.style.color = 'white';
+}
